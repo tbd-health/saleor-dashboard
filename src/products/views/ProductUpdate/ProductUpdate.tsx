caea464f84cf2df86e84a1826dbdb36cf11655f9
Dominik Żegleń <flesz3@o2.pl>
2022-05-11 15:08:31 +0200

    Add actions column

--- src/products/views/ProductUpdate/ProductUpdate.tsx
+++ src/products/views/ProductUpdate/ProductUpdate.tsx
@@ -594,6 +594,11 @@ export const ProductUpdate: React.FC<ProductUpdateProps> = ({ id, params }) => {
         onSubmit={handleSubmit}
         onWarehouseConfigure={() => navigate(warehouseAddPath)}
         onVariantsAdd={() => openModal("add-variants")}
+        onVariantShow={variantId =>
+          navigate(productVariantEditUrl(product.id, variantId), {
+            resetScroll: true
+          })
+        }
         onVariantReorder={handleVariantReorder}
         onVariantEndPreorderDialogOpen={() => setIsEndPreorderModalOpened(true)}
         onImageUpload={handleImageUpload}
